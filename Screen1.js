1a7fcbcb4d0895772385c4768a9692ebaed1453b
quangthao2002 <quangthao22122002@gmail.com>
2023-11-05 02:24:46 +0700

    doneScreen2api

--- Screen1.js
+++ Screen1.js
@@ -1,14 +1,171 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Pressable,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+// import { Icon } from "react-native-vector-icons/icon";
 
-const Screen1 = () => {
+function Screen1() {
+  var [data, setData] = useState([]);
+  const [text, setText] = useState("");
+  const navigation = useNavigation();
+  useEffect(() => {
+    fetch("https://654196bff0b8287df1fe827b.mockapi.io/bonus")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+      });
+  }, []);
   return (
-    <View>
-      <Text>Screen1</Text>
+    <View style={styles.container}>
+      <View style={{ marginVertical: 10 }}>
+        <Text style={{ color: "gray", fontSize: 18 }}>Welcome, Jala!</Text>
+        <Text style={{ fontSize: 22, fontWeight: "bold", marginBottom: 10 }}>
+          Choice you Best food
+        </Text>
+
+        <TextInput
+          style={{
+            height: 45,
+            borderWidth: "1px",
+            borderColor: "gray",
+            // borderColor:"#D3D3D3",
+            fontSize: "16px",
+            fontWeight: "bold",
+            width: "70%",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#D3D3D3",
+          }}
+          placeholder="   Search Food!"
+          placeholderTextColor="gray"
+          onChangeText={(newText) => setText(newText)}
+          defaultValue={text}
+        />
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <Pressable
+            style={{
+              borderWidth: 1,
+              borderColor: "gray",
+              width: 90,
+              backgroundColor: "yellow",
+              height: "30px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Donut
+          </Pressable>
+          <Pressable
+            style={{
+              borderWidth: 1,
+              borderColor: "gray",
+              width: 90,
+              marginLeft: 20,
+              height: "30px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Pink Donut
+          </Pressable>
+          <Pressable
+            style={{
+              borderWidth: 1,
+              borderColor: "gray",
+              width: 90,
+              marginLeft: 20,
+              height: "30px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Floating
+          </Pressable>
+        </View>
+      </View>
+      {data.map((item) => {
+        return (
+          <View key={item.id}
+            style={{
+              flexDirection: "row",
+              marginTop: 20,
+              backgroundColor: "pink",
+              borderRadius: 10,
+              paddingHorizontal: 10,
+              marginRight: 10,
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "white",
+                margin: 3,
+                borderRadius: 10,
+                width: 100,
+                height: 100,
+              }}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+              />
+            </View>
+            <View style={{ marginLeft: 10 }}>
+              <Text style={{ ...styles.title, marginBottom: 5 }}>
+                {item.title}
+              </Text>
+              <Text style={{ ...styles.desc, marginBottom: 5 }}>
+                {item.description}
+              </Text>
+              <Text style>{item.price}</Text>
+            </View>
+            <View
+              style={{
+                position: "absolute",
+                right: 0,
+                bottom: 0,
+                backgroundColor: "#F1B000",
+                padding: 2,
+                borderRadius: 30,
+                borderTopLeftRadius: 90,
+              }}
+            >
+              <Pressable onPress={()=>{
+                  navigation.navigate("Screen2",{item:item})
+              }}>
+                <Ionicons name="add" size={40} color={"white"} />
+              </Pressable>
+            </View>
+          </View>
+        );
+      })}
     </View>
-  )
+  );
 }
 
-export default Screen1
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingLeft: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  desc: {
+    color: "gray",
+    fontSize: 18,
+    fontWeight: "400",
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Screen1;
---
+++ Screen2.js
@@ -0,0 +1,83 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Ionicons, SimpleLineIcons } from "@expo/vector-icons";
+const Screen2 = ({ route }) => {
+  const { item } = route.params;
+  const [number, setNumber] = useState(1);
+  const numberup = () => {
+    setNumber(number + 1);
+  };
+  const numberdown = () => {
+    if (number > 1) {
+      setNumber(number - 1);
+    }
+  };
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 4, alignItems: "center", marginVertical: 30 }}>
+        <Image
+          source={{ uri: item.image }}
+          style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+        />
+      </View>
+      <View style={{ flex: 6 , marginHorizontal:20}}>
+        <Text style={{ fontWeight: "800", fontSize: 18,marginBottom:10 }}>{item.title}</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between",marginBottom:10 }}>
+          <Text style={{ fontWeight: "800", fontSize: 14, color: "gray" }}>
+            {item.description}
+          </Text>
+          <Text style={{ fontWeight: "800", fontSize: 18 }}>{item.price}</Text>
+        </View>
+
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Ionicons name="time-outline" size={30} />
+          <Text style={{ color: "gray", marginLeft: 5 }}>Delivery in</Text>
+        </View>
+
+        <View style={{ flexDirection: "row" ,justifyContent:"space-between"}}>
+          <Text style={{ fontWeight: "bold", marginLeft: 40 ,fontSize:18}}>30 min</Text>
+          <View style={{flexDirection:"row"}}>
+          <TouchableOpacity
+            onPress={() => {
+              numberup();
+            }}
+            style={{
+              backgroundColor: "#F1B000",
+              marginLeft: 60,
+              padding: 2,
+              borderRadius: 10,
+            }}
+          >
+            <SimpleLineIcons name="plus" size={20} color={"white"} />
+          </TouchableOpacity>
+          <Text style={{ paddingHorizontal: 10 }}>{number}</Text>
+          <TouchableOpacity
+            style={{ backgroundColor: "#F1B000", padding: 2, borderRadius: 10 }}
+            onPress={() => {
+              numberdown();
+            }}
+          >
+            <SimpleLineIcons name="minus" size={20} color={'white'} />
+          </TouchableOpacity>
+          </View>
+
+        </View>
+          <Text style={{fontWeight:"bold",fontSize:18,marginBottom:5}}>Restaurants info</Text>
+          <Text>
+            Order a Large Pizza but the size is the equivalent of a medium/small
+            from other places at the same price range.
+          </Text>
+
+        <View style={{width:'100%',height:60, backgroundColor:"#F1B000", borderRadius:10,marginTop:20}}>
+          <Pressable style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+           <Text style={{color:'white',fontSize:18,fontWeight:"bold"}}>Add to card</Text> 
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Screen2;
+
+const styles = StyleSheet.create({});
